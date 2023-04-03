112014735a0b0e2b187d3b20c7776aafde85834d
Ayush <ayush.vanparia@sanskartechnolab.in>
2023-04-03 19:41:15 +0530

    Second Commit added hr and updated data

--- prac2/prac2/page/page_2/page_2.js
+++ prac2/prac2/page/page_2/page_2.js
@@ -10,12 +10,13 @@ function getPurchaseOrders(startDate, endDate) {
         callback: function(data) {
             var rows = '';
             $.each(data.message, function(i, d) {
-                rows += '<tr><td>' + d.name + '</td><td>' + d.supplier + '</td><td>' + d.supplier_name + '</td><td>' + d.transaction_date + '</td><td>' + d.shedule_date + '</td><td>' + d.company + '</td><td>' + d.buying_price_list + '</td><td>' + d.price_list_currency + '</td><td>' + d.total_qty + '</td><td>' + d.total + '</td></tr>';
+                rows += '<tr><td>' + (d.name ? d.name : '') + '</td><td>' + (d.supplier ? d.supplier : '') + '</td><td>' + (d.supplier_name ? d.supplier_name : '') + '</td><td>' + (d.transaction_date ? d.transaction_date : '') + '</td><td>' + (d.shedule_date ? d.shedule_date : '') + '</td><td>' + (d.company ? d.company : '') + '</td><td>' + (d.buying_price_list ? d.buying_price_list : '') + '</td><td>' + (d.price_list_currency ? d.price_list_currency : '') + '</td><td>' + (d.total_qty ? d.total_qty : '') + '</td><td>' + (d.total ? d.total : '') + '</td></tr>';
             });
             $('#po-table tbody').html(rows);
         }
     });
 }
+//             rows += '<tr><td>' + (d.name ? d.name : '') + '</td><td>' + (d.supplier ? d.supplier : '') + '</td><td>' + (d.supplier_name ? d.supplier_name : '') + '</td><td>' + (d.transaction_date ? d.transaction_date : '') + '</td><td>' + (d.shedule_date ? d.shedule_date : '') + '</td><td>' + (d.company ? d.company : '') + '</td><td>' + (d.buying_price_list ? d.buying_price_list : '') + '</td><td>' + (d.price_list_currency ? d.price_list_currency : '') + '</td><td>' + (d.total_qty ? d.total_qty : '') + '</td><td>' + (d.total ? d.total : '') + '</td></tr>';
 
 // =================================================Getting Sales Order Data===============================================================
 function getSalesOrders(startDate, endDate) {
@@ -28,20 +29,61 @@ function getSalesOrders(startDate, endDate) {
         callback: function(data) {
             var rows = '';
             $.each(data.message, function(i, d) {
-                rows += '<tr><td>' + d.name + '</td><td>' + d.customer + '</td><td>' + d.customer_name + '</td><td>' + d.transaction_date + '</td><td>' + d.delivery_date + '</td><td>' + d.company + '</td><td>' + d.currency + '</td><td>' + d.selling_price_list + '</td><td>' + d.total_qty + '</td><td>' + d.total + '</td></tr>';
+				rows += '<tr><td>' + (d.name ? d.name : '') + '</td><td>' + (d.customer ? d.customer : '') + '</td><td>' + (d.customer_name ? d.customer_name : '') + '</td><td>' + (d.transaction_date ? d.transaction_date : '') + '</td><td>' + (d.delivery_date ? d.delivery_date : '') + '</td><td>' + (d.company ? d.company : '') + '</td><td>' + (d.currency ? d.currency : '') + '</td><td>' + (d.selling_price_list ? d.selling_price_list : '') + '</td><td>' + (d.total_qty ? d.total_qty : '') + '</td><td>' + (d.total ? d.total : '') + '</td></tr>';
+
             });
             $('#so-table tbody').html(rows);
         }
     });
 }
+// ==================================================Getting Card Attendance details========================================================
 
+function getCarddAttendance(startDate, endDate) {
+    frappe.call({
+        method: 'prac2.prac2.page.page_2.page_2.get_card_attendance',
+        args: {
+            start_date: startDate,
+            end_date: endDate
+        },
+        callback: function(data) {
+            var rows = '';
+            $.each(data.message, function(i, d) {
+				rows += '<tr><td>' + (d.employee ? d.employee : '') + '</td><td>' + (d.employee_name ? d.employee_name : '') + '</td><td>' + (d.card_status ? d.card_status : '') + '</td><td>' + (d.card_working_hours ? d.card_working_hours : '') + '</td><td>' + (d.attendance_date ? d.attendance_date : '') + '</td><td>' + (d.company ? d.company : '') + '</td><td>' + (d.department ? d.department : '') + '</td><td>' + (d.shift ? d.shift : '') + '</td><td>' + (d.in_time ? d.in_time : '') + '</td><td>' + (d.out_time ? d.out_time : '') + '</td></tr>';
+
+            });
+            $('#ca-table tbody').html(rows);
+        }
+    });
+}
+
+
+
+// ===================================================Getting stock entry details============================================================
+
+function getStockEntry(startDate, endDate) {
+    frappe.call({
+        method: 'prac2.prac2.page.page_2.page_2.get_stock_entry',
+        args: {
+            start_date: startDate,
+            end_date: endDate
+        },
+        callback: function(data) {
+            var rows = '';
+            $.each(data.message, function(i, d) {
+				rows += '<tr><td>' + (d.name ? d.name : '') + '</td><td>' + (d.posting_date ? d.posting_date : '') + '</td><td>' + (d.posting_time ? d.posting_time : '') + '</td><td>' + (d.stock_entry_type ? d.stock_entry_type : '') + '</td><td>' + (d.company ? d.company : '') + '</td><td>' + (d.shift_type ? d.shift_type : '') + '</td><td>' + (d.from_warehouse ? d.from_warehouse : '') + '</td><td>' + (d.to_warehouse ? d.to_warehouse : '') + '</td><td>' + (d.total_qty ? d.total_qty : '') + '</td><td>' + (d.total_outgoing_value ? d.total_outgoing_value : '') + '</td></tr>';
+
+            });
+            $('#st-table tbody').html(rows);
+        }
+    });
+}
 
 // =======================================================Frappe Page declaration============================================================
 
 frappe.pages['page-2'].on_page_load = function(wrapper) {
 	var page = frappe.ui.make_app_page({
         parent: wrapper,
-        title: 'Data',
+        title: 'Report',
         single_column: true
     });
 	
@@ -54,8 +96,8 @@ frappe.pages['page-2'].on_page_load = function(wrapper) {
 				</div>\
 				<div class="col">\
 					<input type="date"  class="form-control"  id="start-date">\
-				</div>\
-				</div>\
+					</div>\
+					</div>\
 				</div>\
 				<div class=" col ">\
 				<div class="row">\
@@ -64,63 +106,113 @@ frappe.pages['page-2'].on_page_load = function(wrapper) {
 				</div>\
 				<div class="col">\
 					<input type="date"  class="form-control " id="end-date">\
-				</div>\
+					</div>\
 			</div>\
 			</div>\
 	</div>\
     ');
-
+	
 	
 // =======================================================Purchase Table =====================================================================
 
-	var po = $('<h3 class="mt-5 text-center ">Purchase Order</h3>')
-	
-    var po_table = $('<table class="table table-bordered" id="po-table">\
+var po = $('<h3 class="mt-5 text-center ">Purchase</h3>')
+
+var po_table = $('<table class="table table-bordered" id="po-table">\
         <thead>\
 		<tr>\
-				<th>Name</th>\
-                <th>Supplier</th>\
-				<th>Supplier Name</th>\
-				<th>Transaction Date</th>\
-				<th>Schedule Date</th>\
-				<th>Company</th>\
-				<th>Buying Price List</th>\
-				<th>Price List Currency</th>\
-				<th>Total QTY</th>\
+		<th>Name</th>\
+		<th>Supplier</th>\
+		<th>Supplier Name</th>\
+		<th>Transaction Date</th>\
+		<th>Schedule Date</th>\
+		<th>Company</th>\
+		<th>Buying Price List</th>\
+		<th>Price List Currency</th>\
+		<th>Total QTY</th>\
                 <th>Total</th>\
             </tr>\
         </thead>\
         <tbody>\
         </tbody>\
     </table>');
-// ================================================================Sales Table==============================================================
 
-var so = $('<h3 class="mt-5 text-center">Sales Order</h3>')
-var so_table = $('<table class="table table-bordered" id="so-table">\
+	
+// ================================================================Sales Table==============================================================
+	
+	var so = $('<h3 class="mt-5 text-center">Sales</h3>')
+	var so_table = $('<table class="table table-bordered" id="so-table">\
+<thead>\
+<tr>\
+<th>Name</th>\
+<th>Customer</th>\
+<th>Customer Name</th>\
+<th>Transaction Date</th>\
+<th>Delivery Date</th>\
+<th>Company</th>\
+<th>Currency</th>\
+<th> Selling Price List </th>\
+<th>Total QTY</th>\
+<th>Total</th>\
+</tr>\
+</thead>\
+<tbody>\
+</tbody>\
+</table>'
+);
+// ================================================================Card Attendance ========================================================
+
+// employee, employee_name, card_status, card_working_hours, attendance_date, company, department, shift, in_time, out_time
+
+var ca = $('<h3 class="mt-5 text-center ">HR</h3>')
+
+var ca_table = $('<table class="table table-bordered" id="ca-table">\
+		<thead>\
+		<tr>\
+		<th>Employee</th>\
+		<th>Emplpyee Name</th>\
+		<th>Card Status</th>\
+		<th>Card Working Hours</th>\
+		<th>Attendance Date</th>\
+		<th>Company</th>\
+		<th>Department</th>\
+		<th>Shift</th>\
+		<th>in_time</th>\
+		<th>out_time</th>\
+			</tr>\
+		</thead>\
+		<tbody>\
+		</tbody>\
+	</table>');
+
+// ===================================================Stock Entry Table===================================================================
+// name, posting_date, posting_time, stock_entry_type, company, shift_type, from_warehouse, to_warehouse, total_qty, total_outgoing_value, total_incoming_value
+
+var stock_entry = $('<h3 class="mt-5 text-center">Stock Entry</h3>')
+var st_table = $('<table class="table table-bordered" id="st-table">\
 <thead>\
 		<tr>\
 				<th>Name</th>\
-                <th>Customer</th>\
-				<th>Customer Name</th>\
-				<th>Transaction Date</th>\
-				<th>Delivery Date</th>\
+                <th>Posting Date</th>\
+				<th>Posting Time</th>\
+				<th>Stock Entry Type</th>\
 				<th>Company</th>\
-				<th>Currency</th>\
-				<th> Selling Price List </th>\
+				<th>Shift Type</th>\
+				<th>From Warehouse</th>\
+				<th> To Warehouse </th>\
 				<th>Total QTY</th>\
-                <th>Total</th>\
+                <th>Total Outgoing Value</th>\
+                <th>Total Incoming Value</th>\
 				</tr>\
         </thead>\
         <tbody>\
         </tbody>\
 		</table>'
 		);
-		
-		
+
 // ===================================================Adding tables to page================================================================
 		
 		
-		page.main.append(filters, po, po_table, so, so_table);
+		page.main.append(filters, po, po_table, so, so_table,ca, ca_table, stock_entry, st_table);
 		
 //======================================================= Filtering Dates==================================================================
 	
@@ -130,10 +222,40 @@ var so_table = $('<table class="table table-bordered" id="so-table">\
 			var endDate = $('#end-date').val();
 			getPurchaseOrders(startDate, endDate);
 			getSalesOrders(startDate, endDate);
+			getStockEntry(startDate, endDate);
+			getCarddAttendance(startDate,endDate);
 		});
 		
 		wrapper.page.set_primary_action('Print', function() {
-			window.print();
+
+			window.print()
+				// var printWindow = window.open('', '', 'height=500,width=800');
+				// printWindow.document.write('<html><head>');
+				// printWindow.document.write('</head><body><div >');
+				// printWindow.document.write('<h3 class="mt-5" style="text-align: center">Purchase Order</h3>');
+				// printWindow.document.write('<table class="table" style="border: 1px solid black">');
+				// printWindow.document.write($('#po-table').html());
+				// printWindow.document.write('</table></div></body></html>');
+				
+				// printWindow.document.write('<br><br>');
+
+				// // printWindow.document.write('<html><head><title>Sales Order Table</title>');
+				// // printWindow.document.write('</head><body>');
+				// // printWindow.document.write('<h3 class="mt-5 text-center ">Sales Order</h3>');
+				// // printWindow.document.write('<table class="table" style="border:1px solid black">');
+				// // printWindow.document.write($('#so-table').html());
+				// // printWindow.document.write('</table></body></html>');
+				
+				// // printWindow.document.write('<br><br>');
+				
+				// // printWindow.document.write('<html><head><title>Card Attendance</title>');
+				// // printWindow.document.write('</head><body>');
+				// // printWindow.document.write('<h3 class="mt-5">Card Attendance</h3>');
+				// // printWindow.document.write('<table class="table">');
+				// // printWindow.document.write($('#ca-table').html());
+				// // printWindow.document.write('</table></body></html>');
+				// printWindow.document.close();
+				// printWindow.print();
 			
 		});
 
--- prac2/prac2/page/page_2/page_2.py
+++ prac2/prac2/page/page_2/page_2.py
@@ -17,4 +17,21 @@ def get_sales_orders(start_date, end_date):
     """, (start_date, end_date), as_dict=True)
 
 
+@frappe.whitelist()
+def get_stock_entry(start_date, end_date):
+    return frappe.db.sql("""
+        SELECT name, posting_date, posting_time, stock_entry_type, company, shift_type, from_warehouse, to_warehouse, total_qty, total_outgoing_value, total_incoming_value
+        FROM `tabStock Entry`
+        WHERE posting_date BETWEEN %s AND %s
+    """, (start_date, end_date), as_dict=True)
+    
+@frappe.whitelist()
+def get_card_attendance(start_date, end_date):
+    return frappe.db.sql("""
+        SELECT employee, employee_name, card_status, card_working_hours, attendance_date, company, department, shift, in_time, out_time
+        FROM `tabCard Attendance`
+        WHERE attendance_date BETWEEN %s AND %s
+    """, (start_date, end_date), as_dict=True)
+
+
 # 'name', 'supplier', 'supplier_name', 'transaction_date', 'schedule_date', 'company', 'buying_price_list', 'price_list_currency', 'set_from_warehouse', 'total_qty']
